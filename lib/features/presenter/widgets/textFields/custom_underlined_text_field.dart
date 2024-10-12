9abfcbdacb69ee135cb35186dd56d07455c69e3f
lenincanuto <lenin@ezsolucoes.com.br>
2024-10-12 17:33:53 -0300

    refactor: adjust textField to receive input formatters and keyboard type

--- lib/features/presenter/widgets/textFields/custom_underlined_text_field.dart
+++ lib/features/presenter/widgets/textFields/custom_underlined_text_field.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:new_ezagro_flutter/consts/app_text_styles.dart';
 
 import '../../../../consts/app_colors.dart';
@@ -8,12 +9,16 @@ class CustomUnderlinedTextField extends StatefulWidget {
   final String? hintText;
   final TextEditingController controller;
   final bool passwordField;
+  final TextInputType? inputType;
+  final List<TextInputFormatter>? inputFormatters;
 
   const CustomUnderlinedTextField(
       {super.key,
       required this.onPressed,
       required this.controller,
       this.hintText,
+      this.inputType,
+      this.inputFormatters,
       required this.passwordField});
 
   @override
@@ -22,12 +27,14 @@ class CustomUnderlinedTextField extends StatefulWidget {
 }
 
 class _CustomUnderlinedTextFieldState extends State<CustomUnderlinedTextField> {
+  bool sufixIconPressed = false;
+
   @override
   Widget build(BuildContext context) {
-    bool sufixIconPressed = false;
-
     return TextField(
       controller: widget.controller,
+      inputFormatters: widget.inputFormatters,
+      keyboardType: widget.inputType,
       decoration: InputDecoration(
           hintText: widget.hintText ?? '',
           hintStyle: AppTextStyles.hintTextFieldTextStyle(
@@ -47,6 +54,7 @@ class _CustomUnderlinedTextFieldState extends State<CustomUnderlinedTextField> {
               : null,
           focusedBorder: const UnderlineInputBorder(
               borderSide: BorderSide(color: AppColors.greyColor))),
+      obscureText: (widget.passwordField && !sufixIconPressed),
     );
   }
 }

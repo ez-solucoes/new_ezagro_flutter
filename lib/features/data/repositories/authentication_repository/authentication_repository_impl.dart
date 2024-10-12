b7824d858d5a78a692997d7aea25453fa203727a
lenincanuto <lenin@ezsolucoes.com.br>
2024-10-12 10:12:03 -0300

    feature: recover password repository implementation

--- lib/features/data/repositories/authentication_repository/authentication_repository_impl.dart
+++ lib/features/data/repositories/authentication_repository/authentication_repository_impl.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:new_ezagro_flutter/core/errors/application_error.dart';
 import 'package:new_ezagro_flutter/core/errors/generic_error.dart';
+import 'package:new_ezagro_flutter/core/usecase/empty_result.dart';
 import 'package:new_ezagro_flutter/features/data/datasources/authentication_datasource/authentication_datasource.dart';
 import '../../../domain/entities/authentication_entities/authentication_entity.dart';
 import '../../../domain/params/authentication_params/authentication_params.dart';
@@ -22,6 +23,21 @@ class AuthenticationRepositoryImpl implements AuthenticationRepository {
     } catch (e, stacktrace) {
       return Left(GenericError(
         fingerprint: '$AuthenticationRepositoryImpl.authentication',
+        additionalInfo: stacktrace.toString(),
+      ));
+    }
+  }
+
+  @override
+  Future<Either<ApplicationError, EmptyResult>> recoverPassword(AuthenticationParams authenticationParams) async {
+    try {
+      final result = await datasource.authenticate(authenticationParams);
+      return Right(result);
+    } on ApplicationError catch (e) {
+      return Left(e);
+    } catch (e, stacktrace) {
+      return Left(GenericError(
+        fingerprint: '$AuthenticationRepositoryImpl.recoverPassword',
         additionalInfo: stacktrace.toString(),
       ));
     }
--- lib/features/domain/repositories/authentication_repository/authentication_repository.dart
+++ lib/features/domain/repositories/authentication_repository/authentication_repository.dart
@@ -1,9 +1,13 @@
 import 'package:dartz/dartz.dart';
 import 'package:new_ezagro_flutter/core/errors/application_error.dart';
+import 'package:new_ezagro_flutter/core/usecase/empty_result.dart';
 import '../../entities/authentication_entities/authentication_entity.dart';
 import '../../params/authentication_params/authentication_params.dart';
 
-abstract class AuthenticationRepository{
+abstract class AuthenticationRepository {
+  Future<Either<ApplicationError, AuthenticationEntity>> authentication(
+      AuthenticationParams authenticationParams);
 
-  Future<Either<ApplicationError, AuthenticationEntity>> authentication(AuthenticationParams authenticationParams);
+  Future<Either<ApplicationError, EmptyResult>> recoverPassword(
+      AuthenticationParams authenticationParams);
 }

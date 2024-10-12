9382ff0456cde2dd371cb1f34a556392b84621e5
lenincanuto <lenin@ezsolucoes.com.br>
2024-10-12 17:24:20 -0300

    feature: recover password and repository implementation

--- lib/features/data/repositories/authentication_repository/authentication_repository_impl.dart
+++ lib/features/data/repositories/authentication_repository/authentication_repository_impl.dart
@@ -2,10 +2,11 @@ import 'package:dartz/dartz.dart';
 import 'package:new_ezagro_flutter/core/errors/application_error.dart';
 import 'package:new_ezagro_flutter/core/errors/generic_error.dart';
 import 'package:new_ezagro_flutter/core/usecase/empty_result.dart';
-import 'package:new_ezagro_flutter/features/data/datasources/authentication_datasource/authentication_datasource.dart';
+
 import '../../../domain/entities/authentication_entities/authentication_entity.dart';
 import '../../../domain/params/authentication_params/authentication_params.dart';
 import '../../../domain/repositories/authentication_repository/authentication_repository.dart';
+import '../../datasources/authentication_datasource/authentication_datasource.dart';
 
 class AuthenticationRepositoryImpl implements AuthenticationRepository {
   final AuthenticationDatasource datasource;
@@ -13,7 +14,7 @@ class AuthenticationRepositoryImpl implements AuthenticationRepository {
   AuthenticationRepositoryImpl(this.datasource);
 
   @override
-  Future<Either<ApplicationError, AuthenticationEntity>> authentication(
+  Future<Either<ApplicationError, AuthenticationEntity>> authenticate(
       AuthenticationParams authenticationParams) async {
     try {
       final result = await datasource.authenticate(authenticationParams);
@@ -29,9 +30,10 @@ class AuthenticationRepositoryImpl implements AuthenticationRepository {
   }
 
   @override
-  Future<Either<ApplicationError, EmptyResult>> recoverPassword(AuthenticationParams authenticationParams) async {
+  Future<Either<ApplicationError, EmptyResult>> recoverPassword(
+      AuthenticationParams authenticationParams) async {
     try {
-      final result = await datasource.authenticate(authenticationParams);
+      final result = await datasource.recoverPassword(authenticationParams);
       return Right(result);
     } on ApplicationError catch (e) {
       return Left(e);

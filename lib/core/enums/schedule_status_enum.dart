32eabcc5335e59a764d17b546363a18e8b8087c2
lenincanuto <lenin@ezsolucoes.com.br>
2024-10-18 14:58:57 -0300

    refactor: schedule_status_enum.dart

---
+++ lib/core/enums/schedule_status_enum.dart
@@ -0,0 +1,31 @@
+//[ SCHEDULED, CANCELED, FINISHED ]
+
+enum ScheduleStatusEnum {
+  scheduled,
+  canceled,
+  finished,
+}
+
+scheduleStatusEnum(String type) {
+  switch (type) {
+    case 'SCHEDULED':
+      return ScheduleStatusEnum.scheduled;
+    case 'CANCELED':
+      return ScheduleStatusEnum.canceled;
+    case 'FINISHED':
+      return ScheduleStatusEnum.finished;
+  }
+}
+
+scheduleStatusString(type) {
+  switch (type) {
+    case ScheduleStatusEnum.scheduled:
+      return 'SCHEDULED';
+    case ScheduleStatusEnum.canceled:
+      return 'CANCELED';
+    case ScheduleStatusEnum.finished:
+      return 'FINISHED';
+    default:
+      return '';
+  }
+}

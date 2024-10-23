de63db34d697c8dc689af01528d41fcc94e65861
lenincanuto <lenin@ezsolucoes.com.br>
2024-10-23 11:48:16 -0300

    feature: build SnackBar to return success, error or alert

---
+++ lib/design_system/widgets/snackbars/custon_snack_bar_widget.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:new_ezagro_flutter/design_system/colors/app_colors.dart';
+import 'package:new_ezagro_flutter/design_system/drawables/app_drawables.dart';
+
+enum SnackBarType {
+  success,
+  error,
+  alert,
+}
+
+class CustomSnackBarWidget {
+  final SnackBarType type;
+  final String message;
+  final Color backgroundColor;
+  final SvgPicture icon;
+
+  CustomSnackBarWidget({
+    required this.type,
+    required this.message,
+    required this.backgroundColor,
+    required this.icon,
+  });
+
+  static show(SnackBarType type, BuildContext context, String message){
+    switch (type) {
+      case SnackBarType.error :
+        return _buildErrorSnackBar(context, message);
+      case SnackBarType.success :
+        return _buildSuccessSnackBar(context, message);
+      case SnackBarType.alert :
+        return _buildAlertSnackBar(context, message);
+      default :
+        throw Exception('SnackBar type not supported');
+    }
+  }
+
+  static _buildSuccessSnackBar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      behavior: SnackBarBehavior.floating,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+      content: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(message),
+          Spacer(),
+      SvgPicture.asset(AppDrawables.checkWhiteIcon),
+        ],
+      ),
+      backgroundColor: AppColors.greenColor,
+    ));
+  }
+
+  static _buildErrorSnackBar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      behavior: SnackBarBehavior.floating,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+      content: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(message),
+          Spacer(),
+          SvgPicture.asset(AppDrawables.closeWhiteIcon),
+        ],
+      ),
+      backgroundColor: AppColors.redColor,
+    ));
+  }
+
+  static _buildAlertSnackBar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      behavior: SnackBarBehavior.floating,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+      content: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(message),
+          Spacer(),
+          SvgPicture.asset(AppDrawables.warningWhiteIcon, color: Colors.white,),
+        ],
+      ),
+      backgroundColor: AppColors.yellowColor,
+    ));
+  }
+}

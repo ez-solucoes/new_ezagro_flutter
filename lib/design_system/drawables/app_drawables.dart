c5cc067e821b67b5ceb5201d858f4709f6d8afbf
lenincanuto <lenin@ezsolucoes.com.br>
2024-10-23 11:46:35 -0300

    feature: insert icon assets to be used on SnackBars

--- lib/design_system/drawables/app_drawables.dart
+++ lib/design_system/drawables/app_drawables.dart
@@ -6,12 +6,12 @@ class AppDrawables {
   static const String logoBaseDir = 'assets/logo/';
 
   //Logos
-  static const String ezAgroLogoIcon =
-      '${logoBaseDir}logo_icon_transparent.svg';
-  static const String ezAgroLogoComplete =
-      '${logoBaseDir}logo_complete_transparent.svg';
+  static const String ezAgroLogoIcon = '${logoBaseDir}logo_icon_transparent.svg';
+  static const String ezAgroLogoComplete = '${logoBaseDir}logo_complete_transparent.svg';
 
   //Icons
-  static const String checkCircleColorIcon =
-      '${iconBaseDir}check_circle_color_icon.svg';
+  static const String checkCircleColorIcon = '${iconBaseDir}check_circle_color_icon.svg';
+  static const String closeWhiteIcon = '${iconBaseDir}close_white_icon.svg';
+  static const String warningWhiteIcon = '${iconBaseDir}warning_white_icon.svg';
+  static const String checkWhiteIcon = '${iconBaseDir}check_white_icon.svg';
 }

5f18e3ac845b9ab394e5c4f3e90d2a38fc4967bb
lenincanuto <lenin@ezsolucoes.com.br>
2024-11-06 11:45:51 -0300

    EAW-669 feature: Create Flutter Error Handler that's show to the user

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,14 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:new_ezagro_flutter/app_module.dart';
 import 'package:new_ezagro_flutter/app_widget.dart';
 
+import 'features/presenter/widgets/errors/flutter_error_widget.dart';
+
 void main() async {
+  FlutterError.onError = (FlutterErrorDetails details) {
+    FlutterError.dumpErrorToConsole(details);
+    runApp(FlutterErrorWidget(details));
+  };
+
   runApp(
     ModularApp(
       module: AppModule(),

4703c31e7d7163840b0f011d58ad0181135476a5
lenincanuto <lenin@ezsolucoes.com.br>
2024-11-13 08:43:46 -0300

    EAW-669 feature: Create Splash Screen for native Android and iOS devices

--- lib/app_module.dart
+++ lib/app_module.dart
@@ -168,7 +168,7 @@ class AppModule extends Module {
 
   @override
   void routes(RouteManager r) {
-    r.child(AppRoutes.appDefaultPage, child: (context) => LoginPage());
+    r.child(AppRoutes.appDefaultPage, child: (context) => SplashPage());
     r.child(AppRoutes.appSplashPage, child: (context) => const SplashPage());
 
     r.child(AppRoutes.appLoginPage, child: (context) => LoginPage());

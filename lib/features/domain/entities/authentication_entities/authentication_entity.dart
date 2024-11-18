13b9012aa25e647ff3f9d06f000d691dcafb8ffd
lenincanuto <lenin@ezsolucoes.com.br>
2024-11-18 13:30:02 -0300

    Refactor: Adjust authentication_entity.dart to the new structure of api return

--- lib/features/domain/entities/authentication_entities/authentication_entity.dart
+++ lib/features/domain/entities/authentication_entities/authentication_entity.dart
@@ -1,6 +1,7 @@
 import 'package:equatable/equatable.dart';
 import 'package:new_ezagro_flutter/features/domain/entities/company_entities/company_entity.dart';
 import 'package:new_ezagro_flutter/features/domain/entities/group_entitites/group_entity.dart';
+import 'package:new_ezagro_flutter/features/domain/entities/user_entities/user_entity.dart';
 
 import '../client_entities/client_entity.dart';
 import '../employee_entities/employee_entity.dart';
@@ -9,42 +10,18 @@ import '../outsource_entities/outsource_entity.dart';
 class AuthenticationEntity extends Equatable {
   final int? id;
   final String? token;
-  final String? username;
-  final ClientEntity? client;
-  final EmployeeEntity? employee;
-  final OutsourceEntity? outsource;
-  final CompanyEntity? company;
-  final String? dtLastLogin;
-  final bool? isLocked;
-  final String? firstAccessVerification;
-  final List<GroupEntity>? group;
+  final UserEntity? user;
 
   const AuthenticationEntity({
     this.id,
     this.token,
-    this.client,
-    this.username,
-    this.employee,
-    this.outsource,
-    this.company,
-    this.dtLastLogin,
-    this.isLocked,
-    this.firstAccessVerification,
-    this.group,
+    this.user,
   });
 
   @override
   List<Object?> get props => [
         id,
         token,
-        client,
-        username,
-        employee,
-        outsource,
-        company,
-        dtLastLogin,
-        isLocked,
-        firstAccessVerification,
-        group,
+        user,
       ];
 }

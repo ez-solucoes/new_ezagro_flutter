29a274d461edf30ae94b2a2149d4088cddf3cd42
lenincanuto <lenin@ezsolucoes.com.br>
2024-12-04 19:57:31 -0300

    feature: first change password and access flux

--- lib/app_module.dart
+++ lib/app_module.dart
@@ -235,7 +235,7 @@ class AppModule extends Module {
 
   @override
   void routes(RouteManager r) {
-    r.child(AppRoutes.appDefaultPage, child: (context) => LoginPage());
+    r.child(AppRoutes.appDefaultPage, child: (context) => RegisterFirstStepPage());
     r.child(AppRoutes.appSplashPage, child: (context) => const SplashPage());
 
     r.child(AppRoutes.appLoginPage, child: (context) => LoginPage());

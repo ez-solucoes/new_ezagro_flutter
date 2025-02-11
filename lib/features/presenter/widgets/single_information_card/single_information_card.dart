b174eb28f55b0c8c2e794decc0fc676dd5a2744d
lenincanuto <lenin@ezsolucoes.com.br>
2025-02-11 00:46:04 -0300

    feature: register render flex adjust from data

--- lib/features/presenter/widgets/single_information_card/single_information_card.dart
+++ lib/features/presenter/widgets/single_information_card/single_information_card.dart
@@ -22,7 +22,7 @@ class SingleInformationCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(4),
         boxShadow: [
           BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
+            color: Colors.grey.withValues(alpha: 0.5),
             spreadRadius: 2,
             blurRadius: 5,
             offset: const Offset(0, 3),

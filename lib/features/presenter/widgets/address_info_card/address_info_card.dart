0258c541825c1328ecfe444377fb50e9f6a8f4cc
lenincanuto <lenin@ezsolucoes.com.br>
2025-07-04 12:02:38 -0300

    refactor: update address_info_card to receive icon and logical tap

--- lib/features/presenter/widgets/address_info_card/address_info_card.dart
+++ lib/features/presenter/widgets/address_info_card/address_info_card.dart
@@ -5,8 +5,15 @@ import '../../../../design_system/colors/app_colors.dart';
 
 class AddressInfoCard extends StatelessWidget {
   final List<Map<String, dynamic>> addressInfo;
+  final IconData? trailingIcon;
+  final VoidCallback? onIconTap;
 
-  const AddressInfoCard({super.key, required this.addressInfo});
+  const AddressInfoCard({
+    super.key,
+    required this.addressInfo,
+    this.onIconTap,
+    this.trailingIcon,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -25,32 +32,51 @@ class AddressInfoCard extends StatelessWidget {
       ),
       child: Padding(
         padding: const EdgeInsets.all(8),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Row(
           children: [
-            ...addressInfo.map((info) {
-              final label = info['label']?.toString() ?? '';
-              final value = info['value']?.toString() ?? '';
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ...addressInfo.map((info) {
+                    final label = info['label']?.toString() ?? '';
+                    final value = info['value']?.toString() ?? '';
 
-              return Padding(
-                padding: const EdgeInsets.symmetric(vertical: 4),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      label,
-                      style: AppTextStyles.smallBoldTextOnCardStyle(color: AppColors.primaryBlackColor),
-                    ),
-                    Expanded(
-                      child: Text(
-                        value,
-                        style: AppTextStyles.cardBodyTextStyle(color: AppColors.primaryBlackColor),
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 4),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            label,
+                            style: AppTextStyles.smallBoldTextOnCardStyle(color: AppColors.primaryBlackColor),
+                          ),
+                          Expanded(
+                            child: Text(
+                              value,
+                              style: AppTextStyles.cardBodyTextStyle(color: AppColors.primaryBlackColor),
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                  ],
+                    );
+                  }),
+                ],
+              ),
+            ),
+
+            if(trailingIcon != null)
+              Padding(
+                padding: EdgeInsets.only(left: 10),
+                child: GestureDetector(
+                  onTap: onIconTap,
+                  child: Icon(
+                    trailingIcon,
+                    size: 24,
+                    color: AppColors.primaryBlackColor,
+                  ),
                 ),
-              );
-            }),
+              ),
           ],
         ),
       ),
